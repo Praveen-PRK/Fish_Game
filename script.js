fa64680dc715d18b5a5ef7db8840a41240d16b12
PRK <pr5veen.k8@gmail.com>
2025-01-12 21:05:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Canvas Setup
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+canvas.width = 800;
+canvas.height = 500;
 
 let score = 0;
 let gameFrame = 0;
